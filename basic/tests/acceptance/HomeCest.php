652cdf9e40914cae008acf9ea060f4527c3a6d5f
BrunoRepullo <96079500+BrunoRepullo@users.noreply.github.com>
2022-01-04 00:13:02 +0100

    Creamos la funcion para registrarse.php. Editamos el main.php del layouts para cambios posteriores Editamos ligeramente la portada. Creacion de carpeta imagenes

--- basic/tests/acceptance/HomeCest.php
+++ basic/tests/acceptance/HomeCest.php
@@ -7,7 +7,7 @@ class HomeCest
     public function ensureThatHomePageWorks(AcceptanceTester $I)
     {
         $I->amOnPage(Url::toRoute('/site/index'));        
-        $I->see('My Company');
+        $I->see('Alertas Ciudad');
         
         $I->seeLink('About');
         $I->click('About');
--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -28,13 +28,14 @@ AppAsset::register($this);
 <header>
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        'brandLabel' => 'Alertas Ciudad',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
         ],
     ]);
-    echo Nav::widget([
+
+    /*echo Nav::widget([
         'options' => ['class' => 'navbar-nav ms-auto'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
@@ -45,7 +46,8 @@ AppAsset::register($this);
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
+                ['label' => 'Login', 'url' => ['/site/login']],
+                ['label' => 'Registrarse', 'url' => ['/site/registrarse']],
             ) : (
                 '<li>'
                 . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
@@ -57,12 +59,69 @@ AppAsset::register($this);
                 . '</li>'
             )
         ],
-    ]);
+    ]);*/
+    if(Yii::$app->user->isGuest == FALSE)
+    {
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav ms-auto'],
+            'items' => [
+                ['label' => 'Home', 'url' => ['/site/index']],
+                ['label' => 'Alertas', 'url' => ['/site/alertas']],
+                ['label' => 'Etiquetas', 'url' => ['/site/etiquetas']],
+                ['label' => 'Areas', 'url' => ['/site/areas']],
+                ['label' => 'Incidencias', 'url' => ['/site/incidencias']],
+                ['label' => 'About', 'url' => ['/site/about']],
+                ['label' => 'Contact', 'url' => ['/site/contact']],
+                '<li>'
+                . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
+                . Html::submitButton(
+                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    ['class' => 'btn btn-link logout']
+                )
+                . Html::endForm()
+                . '</li>'
+
+            ],
+        ]);
+    }
+    else
+    {
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav ms-auto'],
+            'items' => [
+                ['label' => 'Home', 'url' => ['/site/index']],
+                ['label' => 'Alertas', 'url' => ['/site/alertas']],
+                ['label' => 'Etiquetas', 'url' => ['/site/etiquetas']],
+                ['label' => 'Areas', 'url' => ['/site/areas']],
+                ['label' => 'Incidencias', 'url' => ['/site/incidencias']],
+                ['label' => 'About', 'url' => ['/site/about']],
+                ['label' => 'Contact', 'url' => ['/site/contact']],
+                ['label' => 'Login', 'url' => ['/site/login']],
+                ['label' => 'Registrarse', 'url' => ['/site/registrarse']],
+            ],
+        ]);
+    }
+
+
     NavBar::end();
+    
     ?>
+
 </header>
 
 <main role="main" class="flex-shrink-0">
+    
+    <div class="container">
+    <?php
+    /* PARTE PRIVADA */
+    if(Yii::$app->user->identity == NULL) { echo "menu moderador";} else {  echo "menu admin";}
+    //if(Yii::$app->user->identity == "moderador") { echo "menu moderador";}
+    //if(Yii::$app->user->identity == "admin") {  echo "menu admin";}
+    //var_dump(Yii::$app->user->identity);
+    
+    ?>
+    </div>
+
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
@@ -70,11 +129,12 @@ AppAsset::register($this);
         <?= Alert::widget() ?>
         <?= $content ?>
     </div>
+
 </main>
 
 <footer class="footer mt-auto py-3 text-muted">
     <div class="container">
-        <p class="float-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="float-left">&copy; Alertas Ciudad <?= date('Y') ?></p>
         <p class="float-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
