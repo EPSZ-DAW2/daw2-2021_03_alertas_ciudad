80500ef6e533d2b04a584b5673ad95dbb4570f43
gaf3700 <gaf3700@usal.es>
2022-01-04 23:39:27 +0100

    creación controlador alertas

---
+++ basic/views/alertas/index.php
@@ -0,0 +1,65 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\AlertasSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Alertas';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="alertas-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Alertas', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'titulo:ntext',
+            'descripcion:ntext',
+            'fecha_inicio',
+            'duracion_estimada',
+            //'direccion:ntext',
+            //'notas_lugar:ntext',
+            //'area_id',
+            //'detalles:ntext',
+            //'notas:ntext',
+            //'url:ntext',
+            //'imagen_id',
+            //'imagen_revisada',
+            //'categoria_id',
+            //'activada',
+            //'visible',
+            //'terminada',
+            //'fecha_terminacion',
+            //'notas_terminacion:ntext',
+            //'num_denuncias',
+            //'fecha_denuncia1',
+            //'bloqueada',
+            //'bloqueo_usuario_id',
+            //'bloqueo_fecha',
+            //'bloqueo_notas:ntext',
+            //'crea_usuario_id',
+            //'crea_fecha',
+            //'modi_usuario_id',
+            //'modi_fecha',
+            //'notas_admin:ntext',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
