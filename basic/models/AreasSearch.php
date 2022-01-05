d7cc6bc7a8b03270f2b4b74ac8b6cb359636896f
MarioLabra <9mlabrav8@usal.es>
2022-01-05 11:21:25 +0100

    Cambio prueba

--- basic/models/AreasSearch.php
+++ basic/models/AreasSearch.php
@@ -5,7 +5,7 @@ namespace app\models;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\models\Areas;
-
+/** Hola que tal
 /**
  * AreasSearch represents the model behind the search form of `app\models\Areas`.
  */
