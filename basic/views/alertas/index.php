5e92bbdb9dabbfa14f80ca8b368f8736304232f0
gaf3700 <gaf3700@usal.es>
2022-01-08 00:44:09 +0100

    función de aleces para id áreas(ya aplicable al resto

--- basic/views/alertas/index.php
+++ basic/views/alertas/index.php
@@ -9,7 +9,10 @@ use yii\grid\GridView;
 
 $this->title = 'Alertas';
 $this->params['breadcrumbs'][] = $this->title;
+
 ?>
+<?//= $model->render('view') ?>
+
 <div class="alertas-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
@@ -25,7 +28,6 @@ $this->params['breadcrumbs'][] = $this->title;
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
             'id',
             'titulo:ntext',
             'descripcion:ntext',
@@ -33,32 +35,69 @@ $this->params['breadcrumbs'][] = $this->title;
             'duracion_estimada',
             'direccion:ntext',
             'notas_lugar:ntext',
-            'area_id',
+            ['class' => 'yii\grid\DataColumn','attribute' => 'area_id','content'=>function ($model){return '<a href="index.php?AlertaComentariosSearch[alerta_id]='.$model->area_id.'&r=alerta-comentarios%2Findex">'.$model->area_id.'</a>';}],
             'detalles:ntext',
             'notas:ntext',
-            //'url:ntext',
-            //'imagen_id',
-            //'imagen_revisada',
-            //'categoria_id',
-            //'activada',
-            //'visible',
-            //'terminada',
-            //'fecha_terminacion',
-            //'notas_terminacion:ntext',
-            //'num_denuncias',
-            //'fecha_denuncia1',
-            //'bloqueada',
-            //'bloqueo_usuario_id',
-            //'bloqueo_fecha',
-            //'bloqueo_notas:ntext',
-            //'crea_usuario_id',
-            //'crea_fecha',
-            //'modi_usuario_id',
-            //'modi_fecha',
-            //'notas_admin:ntext',
+            'url:ntext',
+            'imagen_id',
+            'imagen_revisada',
+            'categoria_id',
+            'activada',
+            'visible',
+            'terminada',
+            'fecha_terminacion',
+            'notas_terminacion:ntext',
+            'num_denuncias',
+            'fecha_denuncia1',
+            'bloqueada',
+            'bloqueo_usuario_id',
+            'bloqueo_fecha',
+            'bloqueo_notas:ntext',
+            'crea_usuario_id',
+            'crea_fecha',
+            'modi_usuario_id',
+            'modi_fecha',
+            'notas_admin:ntext',
 
             ['class' => 'yii\grid\ActionColumn'],
-        ],
+
+            /* todas las pruevas hasta dar con la solucion
+            ['header' => 'area_id',
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{consultar}',
+                'buttons'=>[
+                    'consultar'=>function ($url, $model) {
+                        return Html::a($model->area_id, $url);
+                    },
+                ],
+
+                'urlCreator'=> function ($action, $model, $key, $index) {
+                    if ($action === 'consultar') {
+                        return 'index.php?AlertaComentariosSearch[alerta_id]='.$model->area_id.'&r=alerta-comentarios%2Findex';
+                    }
+                }
+            ],
+             ['attribute' => 'area_id',
+                'class' => 'yii\grid\DataColumn',
+                'content' => function ($url, $model, $key, $index) {
+                        return '<a href="index.php?AlertaComentariosSearch[alerta_id]=22&r=alerta-comentarios%2Findex">hola</a>';},
+            ],
+            /*[
+                'class' => 'yii\grid\DataColumn',
+                'content' => function ($url, $model, $key, $index) {
+                    if ($model->area_id) {
+                        return $model->area_id;
+                    }
+                        
+                        },
+            ],
+            [
+            'attribute' => 'area_id',
+            'content'=>function ($model){
+                return '<a href="index.php?AlertaComentariosSearch[alerta_id]='.$model->area_id.'&r=alerta-comentarios%2Findex">'.$model->area_id.'</a>';
+            }
+
+        ]*/],
     ]); ?>
 
 
