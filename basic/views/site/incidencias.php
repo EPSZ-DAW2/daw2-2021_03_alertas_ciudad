85a6d0e510977d547d4b9a2df1a9b2e91f275e61
Eufen <eufen.deibe@usal.es>
2022-01-08 23:34:43 +0100

    Creacion de archivos de accion en incidencias publicas

--- basic/views/site/incidencias.php
+++ basic/views/site/incidencias.php
@@ -1,13 +1,32 @@
 <?php
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 ?>
 
 <h1>Incidencias del Usuario</h1>
 <ul>
-<?php //foreach ($incidencias as $usuario_incidencias): ?>
+<?php //foreach ($dataProvider->getModels() as $usuario_incidencias): ?>
     <li>
-        <?//= //Html::encode("{$usuario_incidencias->crea_fecha} ({$incidencias->clase_incidencia})") ?>:
+        <?//= Html::encode("{$usuario_incidencias->crea_fecha} ({$incidencias->clase_incidencia})") ?>:
         <?//= $incidencias->texto ?>
     </li>
-<?php //endforeach; ?>
+<?php// endforeach; ?>
 </ul>
+
+<?php
+$usuario_incidencias = User::model()->findByAttributes('id' => Yii::app()->user->id); // Recuperas un profesor mediante el identificador del usuario logueado
+
+if(isset($usuario_incidencias->id))// Considero que en tu modelo TEACHER tienes declarada la relation "relSubjects".
+
+{
+
+     foreach($usuario_incidencias->id as $incidencia)
+
+     {
+
+          echo($incidencia->crea_fecha);
+
+     }
+
+}
+?>
