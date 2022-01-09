7d67413d85b14ba7d524022b47e9650bbb34d3ec
carlosWeb2 <ccrespo@usal.es>
2022-01-09 12:05:06 +0100

    vistas públicas (faltan detalles)

--- basic/views/site/incidencias.php
+++ basic/views/site/incidencias.php
@@ -1,13 +1,47 @@
 <?php
 use yii\helpers\Html;
-?>
+use yii\grid\GridView;
 
-<h1>Incidencias del Usuario</h1>
-<ul>
-<?php //foreach ($incidencias as $usuario_incidencias): ?>
-    <li>
-        <?//= //Html::encode("{$usuario_incidencias->crea_fecha} ({$incidencias->clase_incidencia})") ?>:
-        <?//= $incidencias->texto ?>
-    </li>
-<?php //endforeach; ?>
-</ul>
+
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\UsuarioIncidenciasSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Incidencias del Usuario';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="usuario-incidencias-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Usuario Incidencias', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'crea_fecha',
+            'clase_incidencia_id',
+            'texto:ntext',
+            'destino_usuario_id',
+            'origen_usuario_id',
+            //'alerta_id',
+            //'comentario_id',
+            //'fecha_lectura',
+            //'fecha_borrado',
+            //'fecha_aceptado',
+
+            ['class' => 'yii\grid\ActionColumn'],  //los botones 
+        ],
+    ]); ?>
+
+
+</div>
