1adc2b5e8bc6b88606b592137116a62ce457231f
MarioLabra <9mlabrav8@usal.es>
2022-01-10 21:07:13 +0100

    Añadido Gestion de Usuarios-Areas Moderacion

---
+++ basic/views/usuarios-area-moderacion/index.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use app\models\Usuarios;
+
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\UsuariosAreaModeracionSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Usuarios Area Moderacions';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="usuarios-area-moderacion-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Usuarios Area Moderacion', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        //'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+           // 'usuario_id',
+           // 'usuario',
+            [
+                'attribute' => 'usuario_id',
+                'label'=> 'Usuario',
+                'value'     => function($model){return Usuarios::findOne($model->usuario_id)->nick; }
+            ],
+
+            'area_id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
